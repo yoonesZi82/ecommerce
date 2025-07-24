c5be2d08d19ea4f6ed02ccd3b355ef076cd6d4cf
yoones <yooneszaamni58@gmail.com>
2025-07-24 14:30:43 +0330

    create address dto

--- src/address/dto/create-address.dto.ts
+++ src/address/dto/create-address.dto.ts
@@ -1,1 +1,31 @@
-export class CreateAddressDto {}
+import { IsNotEmpty, IsOptional, IsString, Matches } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  @IsNotEmpty()
+  province: string;
+
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @IsString()
+  @Matches(/^(\d{10})$/g, {
+    message: 'postal code is must be 10 character',
+  })
+  postal_code: string;
+
+  @IsString()
+  @Matches(/^(\d{11})$/g, {
+    message: 'receiver mobile is must be 10 character',
+  })
+  receiver_mobile: string;
+
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @IsString()
+  @IsOptional()
+  description: string;
+}
